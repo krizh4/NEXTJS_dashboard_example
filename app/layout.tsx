bf8a37067db54c80a60c04cde8b40fc4a3fa9523
Krishmika Abhishek <krishmikaabisheka1@gmail.com>
2025-02-17 16:31:20 +0530

    finished course

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: "The official Next.js course dashboard, built with App router.",
+  // metadataBase: new URL('https://nextjs-dashboard-kappa-puce-46.vercel.app'),
+}
 
 export default function RootLayout({
   children,
